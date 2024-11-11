79b1d5a39beb441f9b8ff85c2d702e40ec13ac60
ani3321r <ani3321r@gmail.com>
2024-11-11 23:20:15 +0530

    err applications, payload capturing...

--- basic concepts/errors_test.zig
+++ basic concepts/errors_test.zig
@@ -7,3 +7,37 @@ const FileErr = error{
   FileNotFound
 };
 
+// applications
+const NoAccess = error{AccessDenied};
+
+test "subset to superset"{
+  const err: FileErr = NoAccess.AccessDenied;
+  try expect(err == FileErr.AccessDenied);
+}
+
+
+// union error type
+// combining error set with another type of error can be achieved
+// using the union error type 
+
+test "error union"{
+  const prob_err: NoAccess!u16 = 15;
+  const no_err = prob_err catch 0;
+
+  try expect(@TypeOf(no_err) == u16);
+  try expect(no_err == 15);
+}
+
+
+// payload capturing
+// funcs often return err unions, |err| receives the value of the error
+fn failingFunc() error{scam}!void{
+  return error.scam;
+}
+
+test "return err"{
+  failingFunc() catch |err|{
+    try expect(err == error.scam);
+    return;
+  };
+}
